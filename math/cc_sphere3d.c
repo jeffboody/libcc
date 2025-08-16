0513a6122ed5da211b1a0d5389a3fda1ddc05b76
Jeff Boody <jeffboody@gmail.com>
2025-08-16 10:53:05 -0600

    add cc_sphere3d

---
+++ math/cc_sphere3d.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2025 Jeff Boody
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#include <math.h>
+#include <stdlib.h>
+
+#define LOG_TAG "cc"
+#include "../cc_log.h"
+#include "cc_sphere3d.h"
+
+/***********************************************************
+* public                                                   *
+***********************************************************/
+
+void cc_sphere3d_load(cc_sphere3d_t* self,
+                      double cx, double cy, double cz,
+                      double r)
+{
+	ASSERT(self);
+
+	cc_vec3d_load(&self->c, cx, cy, cz);
+	self->r = r;
+}
+
+void cc_sphere3d_copy(const cc_sphere3d_t* self,
+                      cc_sphere3d_t* copy)
+{
+	ASSERT(self);
+	ASSERT(copy);
+
+	cc_vec3d_copy(&self->c, &copy->c);
+	copy->r = self->r;
+}
